9193eddbe8bf5cafd1e61be1984676f2e92848c7
JoaoBarreto03 <211039519@aluno.unb.br>
2025-10-09 16:38:23 -0300

    feat: Adicionando sidebar

--- loccar-web/src/app/app.component.ts
+++ loccar-web/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <router-outlet></router-outlet>
-  `
+  imports: [RouterOutlet, SidebarComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {}
--- loccar-web/src/app/layouts/sidebar/sidebar.component.ts
+++ loccar-web/src/app/layouts/sidebar/sidebar.component.ts
@@ -6,73 +6,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   selector: 'app-sidebar',
   standalone: true,
   imports: [CommonModule, RouterLink, RouterLinkActive],
-  template: `
-    <aside class="bg-gray-800 text-white h-screen w-64 fixed left-0 top-0">
-      <div class="p-4">
-        <h2 class="text-2xl font-bold mb-6">Loccar</h2>
-        
-        <nav class="space-y-2">
-          <a 
-            routerLink="/dashboard" 
-            routerLinkActive="bg-gray-700"
-            class="block px-4 py-2 rounded-lg hover:bg-gray-700 transition-colors flex items-center"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" viewBox="0 0 20 20" fill="currentColor">
-              <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-            </svg>
-            Dashboard
-          </a>
-          
-          <a 
-            routerLink="/usuarios" 
-            routerLinkActive="bg-gray-700"
-            class="block px-4 py-2 rounded-lg hover:bg-gray-700 transition-colors flex items-center"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" viewBox="0 0 20 20" fill="currentColor">
-              <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
-            </svg>
-            Usuários
-          </a>
-          
-          <a 
-            routerLink="/veiculos" 
-            routerLinkActive="bg-gray-700"
-            class="block px-4 py-2 rounded-lg hover:bg-gray-700 transition-colors flex items-center"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" viewBox="0 0 20 20" fill="currentColor">
-              <path d="M8 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM15 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
-              <path d="M3 4a1 1 0 00-1 1v10a1 1 0 001 1h1.05a2.5 2.5 0 014.9 0H10a1 1 0 001-1V5a1 1 0 00-1-1H3zM14 7a1 1 0 00-1 1v6.05A2.5 2.5 0 0115.95 16H17a1 1 0 001-1v-5a1 1 0 00-.293-.707l-2-2A1 1 0 0015 7h-1z" />
-            </svg>
-            Veículos
-          </a>
-          
-          <a 
-            routerLink="/locacoes" 
-            routerLinkActive="bg-gray-700"
-            class="block px-4 py-2 rounded-lg hover:bg-gray-700 transition-colors flex items-center"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm2 10a1 1 0 10-2 0v3a1 1 0 102 0v-3zm2-3a1 1 0 011 1v5a1 1 0 11-2 0v-5a1 1 0 011-1zm4-1a1 1 0 10-2 0v7a1 1 0 102 0V8z" clip-rule="evenodd" />
-            </svg>
-            Locações
-          </a>
-        </nav>
-      </div>
-      
-      <div class="absolute bottom-0 left-0 right-0 p-4">
-        <button class="w-full px-4 py-2 text-sm bg-red-600 hover:bg-red-700 rounded-lg transition-colors flex items-center justify-center">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 001 1h12a1 1 0 001-1V4a1 1 0 00-1-1H3zm11 4a1 1 0 10-2 0v4a1 1 0 102 0V7z" clip-rule="evenodd" />
-          </svg>
-          Sair
-        </button>
-      </div>
-    </aside>
-  `,
-  styles: [`
-    :host {
-      display: block;
-    }
-  `]
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {}
