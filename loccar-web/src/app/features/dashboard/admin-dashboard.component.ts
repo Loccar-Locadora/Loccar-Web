e356f3bcce06d26b51bf6c338b341cd6817efc8c
JoaoBarreto03 <211039519@aluno.unb.br>
2025-10-09 17:38:24 -0300

    chore: Configurando tailwind

--- loccar-web/src/app/features/dashboard/admin-dashboard.component.ts
+++ loccar-web/src/app/features/dashboard/admin-dashboard.component.ts
@@ -10,7 +10,7 @@ import { StatItem, ActivityItem } from './models';
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [CommonModule, SidebarComponent, RouterLink, StatsCardComponent],
+  imports: [CommonModule, SidebarComponent, StatsCardComponent],
   templateUrl: './admin-dashboard.component.html',
   styleUrls: ['./admin-dashboard.component.scss']
 })
--- loccar-web/src/app/layouts/stats-card/stats-card.component.ts
+++ loccar-web/src/app/layouts/stats-card/stats-card.component.ts
@@ -1,13 +1,13 @@
-// src/app/shared/stats-card/stats-card.component.ts
 import { Component, Input } from '@angular/core';
-import { StatItem } from '../../features/dashboard/models';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common'; // ← IMPORTANTE
 
 @Component({
   selector: 'app-stats-card',
+  standalone: true,
+  imports: [CommonModule], // ← NECESSÁRIO para *ngIf e *ngFor
   templateUrl: './stats-card.component.html',
   styleUrls: ['./stats-card.component.scss']
 })
 export class StatsCardComponent {
-  @Input() data!: StatItem;
+  @Input() data: any;
 }
